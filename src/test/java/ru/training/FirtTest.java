d1694fef751e8f240d2d63f43dda599be63c0f93
IgorGerasimenko <58310275+IgorGerasimenko@users.noreply.github.com>
2020-06-19 17:32:31 +0400

    Refactor

--- src/test/java/ru/training/FirtTest.java
+++ src/test/java/ru/training/FirtTest.java
@@ -1,18 +1,10 @@
 package ru.training;
 
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.sql.Time;
 
 public class FirtTest extends WebDriverSetting {
     //унаследовались от WebDriverSetting
